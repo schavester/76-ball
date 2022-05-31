04b8fb3c2c9bc75e3753f766a5faeffc956b65f3
Richard Schave <schavester@gmail.com>
2022-05-30 18:46:54 -0700

    fixed menu links and added slashes both sides of URLs for redirect

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ module.exports = {
     return [
       {
         source: '/tags/zippy-the-pinhead/',
-        destination: 'tag/zippy-the-pinhead/',
+        destination: '/tag/zippy-the-pinhead/',
         permanent: true,
       },
             {
         source: '/zippysays/',
-        destination: '2006/06/09/zippysays/',
+        destination: '/2006/06/09/zippysays/',
         permanent: true,
       },
      ]
