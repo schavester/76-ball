c684cdbaf96f35483eb619c74fbb341229ca9372
Richard Schave <schavester@gmail.com>
2022-05-30 19:25:37 -0700

    even more tweaking

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  trailingSlash: true,
   async rewrites() {
     return [
       {
-        source: '/faq2',
-        destination: '/nathan',
+        source: '/faq2/',
+        destination: '/nathan/',
       },
     ]
   },
